425e4c0ca67bde52509e4b005c2fcc5829bef900
jsh94363939-creator <jsh94363939@gmail.com>
2025-11-12 20:46:04 +0900

    25.11.12 수업
    p.154 실습

---
+++ Class 3/3_3.py
@@ -0,0 +1,6 @@
+age=int(input("나이를 입력하세요"))
+height=int(input("키를 입력하세요(단위 cm)"))
+if age < 19 or height < 150:
+    print("입장할 수 없습니다")
+else :
+    print("입장할 수 있습니다")
---
+++ Class 3/3_4.py
@@ -0,0 +1,7 @@
+age=int(input("나이를 입력하시오"))
+if age <10:
+    print("kid")
+elif age >=10 and age < 20:
+    print("youth")
+else :
+    print("adult")
