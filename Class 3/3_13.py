40d9bd99ef0e11ebdf6cf379e1e8f009cb28be13
jsh94363939-creator <jsh94363939@gmail.com>
2026-01-12 12:59:51 +0900

    Delete Class 3 directory

--- Class 3/3_13.py
+++
@@ -1,7 +0,0 @@
-a,b=input("점의 좌표 x,y를 입력하시오 :").split()
-a=int(a)
-b=int(b)
-if (((a**2)-3)+((b**2)-3))**0.5 <=5:
-    print(f"원의 내부에 있음")
-else :
-    print(f"원의 외부에 있음")
--- Class 3/3_7.py
+++
@@ -1,5 +0,0 @@
-game_score=int(input("게임점수를 입력하세요:"))
-if game_score>=1000 :
-    print("고수입니다")
-else :
-    print("입문자입니다")
